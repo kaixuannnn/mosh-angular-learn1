81937997d5a0eee725274b792564cac7915c2ee6
kaixuan <kaixuant@yahoo.com>
2023-02-19 18:18:21 +0800

    ngStyle, style binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,25 +11,5 @@ interface course {
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  courses: course[] = [];
-
-  onLoadCourse() {
-    this.courses = [
-      { id: 1, name: 'course1' },
-      { id: 2, name: 'course2' },
-      { id: 3, name: 'course3' },
-    ];
-  }
-
-  onAdd() {
-    this.courses.push({ id: 4, name: 'course4' });
-  }
-
-  onRemove(course: course) {
-    this.courses = this.courses.filter((item) => item.id !== course.id);
-  }
-
-  trackCourse(index: number, course: course) {
-    return course ? course.id : undefined;
-  }
+  canSave = false;
 }
