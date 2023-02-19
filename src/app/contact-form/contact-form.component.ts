3a1e2a39de749ebfb08f6404e790774ba0453f77
kaixuan <kaixuant@yahoo.com>
2023-02-19 22:28:34 +0800

    ngModel and its class instance

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-contact-form',
   templateUrl: './contact-form.component.html',
-  styleUrls: ['./contact-form.component.css']
+  styleUrls: ['./contact-form.component.css'],
 })
 export class ContactFormComponent {
-
+  log(x: any) {
+    console.log(x);
+  }
 }
